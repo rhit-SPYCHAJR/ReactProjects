84df7eabccd5bf674520324dcd4882c20ada5e2c
rhit-SPYCHAJR <spychajr@rose-hulman.edu>
2023-06-08 21:53:38 -0500

    working with lists

---
+++ src/Lists.js
@@ -0,0 +1,46 @@
+import "./styles.css";
+
+const products = [
+  { title: "Cabbage", amount: "2 heads", priority: "low"},
+  { title: "Garlic", amount: "4 cloves", priority: "low" },
+  { title: "Apple", amount: "3 lbs", priority: "high" },
+  { title: "Bread", amount: "1 loaf", priority: "med" },
+  { title: "Eggs", amount: "2 dozen", priority: "high" }
+];
+
+const priorityLevels = ["low", "med", "high"]
+
+function ListItems() {
+  const items = products.map((product) => (
+    <li>{product.title} ({product.amount})</li>
+  ));
+  return items;
+}
+
+function ListByPriority(){
+  const allCats = priorityLevels.map((priority) => (
+    <div>
+      <h2>{priority} priority</h2>
+      <div>
+        {products.map((product) => (
+          <div>
+          {product.priority == priority && <li>{product.title} ({product.amount})</li>}
+          </div>
+        ))}
+      </div>
+    </div>
+  ));
+
+  return allCats;
+}
+
+export default function MyList() {
+  return (
+    <p>
+      <h1>Shopping list</h1>
+      <div>
+        <ListByPriority />
+      </div>
+    </p>
+  );
+}
