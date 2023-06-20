5a649340a835bf70088a1ca338a77cf24775cabd
Jonathan Spychalski <95193898+rhit-SPYCHAJR@users.noreply.github.com>
2023-06-20 21:16:38 +0000

    support for deleting tasks

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -11,9 +11,6 @@ function AddTask({onClick, visibility}){
     )
 }
 
-
-
-
 function AddTaskInfo({visibility, onSubmit, onBack}){
 
     function handleClick(){
@@ -22,10 +19,10 @@ function AddTaskInfo({visibility, onSubmit, onBack}){
         x[1] = document.getElementById('info').value;
         x[2] = document.getElementById('priority').value;
         x[3] = document.getElementById('due').value;
-        document.getElementById('name').innerHTML = "";
-        document.getElementById('info').innerHTML = "";
-        document.getElementById('priority').innerHTML = "";
-        document.getElementById('due').innerHTML = "";
+        document.getElementById('name').value = "";
+        document.getElementById('info').value = "";
+        document.getElementById('priority').value = "Select a priority";
+        document.getElementById('due').value = "";
         onSubmit(x[0], x[1], x[2], x[3]);
     }
 
@@ -63,7 +60,10 @@ function AddTaskInfo({visibility, onSubmit, onBack}){
     )
 }
 
-function Task({name, priority, due, info}){
+function Task({name, priority, due, info, id, onDelete}){
+    function deleteSelf(){
+        onDelete(id);
+    }
     let color = "white";
     if (priority == "Select a priority") priority = false;
     else switch (priority){
@@ -92,7 +92,15 @@ function Task({name, priority, due, info}){
             {info && <div>Notes: {info}</div>}
             <div className = "label" style={{backgroundColor: color}}>{priority && priority}</div>
             {due && <div>Due date: {due}</div>}
+            <div>Testing: {id}</div>
+            <DeleteTask onClick = {() => {deleteSelf()}}/>
         </div>
+    )
+}
+
+function DeleteTask({onClick}){
+    return (
+        <button onClick = {onClick} className = "delete">x</button>
     )
 }
 
@@ -112,14 +120,32 @@ class Manager extends React.Component{
     }
 
     addTask(name, info, priority, due){
+        let z = i;
+        console.log("i: " + i);
         let x = this.arrayAppend(this.state.tasks, <Task name = {name} 
                                                          info = {info}
                                                          priority = {priority}
                                                          due = {due}
-                                                         key = {i}/>);
+                                                         id = {z}
+                                                         key = {z}
+                                                         onDelete={(q) => this.deleteTask(q)}/>);
         this.setState({addMenuVisible: false,
                        tasks: x});
         i++;
+    }
+
+    deleteTask(id){
+        console.log("before: " + this.state.tasks.map((x) => (x.props.name + "(" + x.props.id + ")")));
+        console.log("id: " + id);
+        let x = this.state.tasks;
+        for(let j = 0; j < x.length; j++){
+            if (x[j].props.id == id){
+                x.splice(j,1);
+                break;
+            }
+        }
+        this.setState({tasks: x});
+        console.log("after: " + this.state.tasks.map((x) => (x.props.name + "(" + x.props.id + ")")));
     }
 
     openAddMenu(){
@@ -132,12 +158,61 @@ class Manager extends React.Component{
         this.setState({addMenuVisible: false});
     }
 
+    getFormattedTask(i){
+        let x = this.state.tasks[i].props;
+        let pr = (x.priority == "Select a priority") ? "" : x.priority;
+        return x.name + "§" +  x.info + "§" + pr + "§" + x.due
+    }
+
+    saveAll(){
+        localStorage.clear();
+        for(let j = 0; j < this.state.tasks.length; j++){
+            localStorage.setItem("task " + j,  this.getFormattedTask(j));
+        }
+        alert("Save successful");
+    }
+
+    loadFromLocal(){
+        let arr = [];
+        let j;
+        for(j = 0;; j++){
+            let x = localStorage.getItem("task " + j);
+            let unpacked;
+            if (x != null){
+                unpacked = x.split("§");
+                arr.push(<Task name = {unpacked[0]} 
+                               info = {unpacked[1]}
+                               priority = {unpacked[2]}
+                               due = {unpacked[3]}
+                               key = {j}
+                               id = {j}
+                               onDelete = {(q) => this.deleteTask(q)}/>);
+            }
+            else break;
+        }
+        i = j;
+        this.setState({tasks: arr});
+        if (arr.length == 0) alert("Local storage does not have any tasks");
+    }
+
+    clearLocal(){
+        localStorage.clear();
+        alert("Clear successful");
+    }
+
     render(){
         return (
             <div>
                 <AddTask visibility = {!this.state.addMenuVisible} onClick = {() => {this.openAddMenu()}}/>
                 <AddTaskInfo visibility = {this.state.addMenuVisible} onSubmit = {(a,b,c,d) => {this.addTask(a,b,c,d)}} onBack = {() => {this.hideAddMenu()}}/>
                 <div>{this.state.tasks}</div>
+                <div className = "toolbar">
+                    <div className = "toolbarHeader">Local storage</div>
+                    {this.state.tasks.length > 0 && <button onClick = {() => {this.saveAll()}}>Save</button>}
+                    <button onClick = {() => {this.loadFromLocal()}}>Load</button>
+                    <button onClick = {() => {this.clearLocal()}}>Clear</button>
+                </div>
+                
             </div>
         )
     }
